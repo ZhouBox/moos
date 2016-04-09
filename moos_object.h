e8f0956442a18f0c150242eb1c637dc109aa6aff
zhou xiang <zhx910322@163.com>
2016-04-09 15:16:53 +0800

    modify get looper

--- moos_object.h
+++ moos_object.h
@@ -10,12 +10,22 @@ DEFINE_NAMESPACE_ZZ_BEGIN
 class MoosObject
 {
 public:
+    MoosObject()
+        : m_looper(Looper::currentLooper())
+    {
+
+    }
+
+
     virtual ~MoosObject() {}
 
     Looper* eventLooper()
     {
-        return Looper::currentLooper();
+        return m_looper;
     }
+
+private:
+    Looper* m_looper;
 };
 
 
