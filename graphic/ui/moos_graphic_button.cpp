a284992b898bcd7556054deafcfa8d783c5516a1
zhouxiang <zhouxiang@pset.suntec.net>
2016-04-13 14:40:01 +0800

    opengl with button

--- graphic/ui/moos_graphic_button.cpp
+++ graphic/ui/moos_graphic_button.cpp
@@ -12,6 +12,7 @@ DEFINE_NAMESPACE_MOOS_BEGIN
 MoosGraphicButton::MoosGraphicButton(MoosGraphicItem* parent)
     : MoosGraphicItem(parent)
 {
+    m_point = MoosPoint(500, 500);
     m_size = MoosSize(120, 45);
 }
 
@@ -21,16 +22,23 @@ void MoosGraphicButton::paint()
     if (!m_parentItem) {
         glutInitWindowPosition(m_point.x(), m_point.y());
         glutInitWindowSize(m_size.width(), m_size.height());
-        glutCreateWindow("Moos Button");
+        m_WindId = glutCreateWindow("Moos Button");
     }
     else {
         m_WindId = glutCreateSubWindow(m_parentItem->windowId(), m_point.x(), m_point.y(),
                                        m_size.width(), m_size.height());
     }
-    glClearColor(0.0, 1.0, 0.0, 1.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glRecti(m_point.x(), m_point.y(), m_point.x() + m_size.width(), m_point.y() + m_size.height());
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    glBegin(GL_POLYGON);
+        glVertex2i(m_point.x(), m_point.y());
+        glVertex2i(m_point.x() + m_size.width(), m_point.y());
+        glVertex2i(m_point.x() + m_size.width(), m_point.y() + m_size.height());
+        glVertex2i(m_point.x(), m_point.y() + m_size.height());
+    glEnd();
+//    glRecti(m_point.x(), m_point.y(), m_point.x() + m_size.width(), m_point.y() + m_size.height());
     MOOS_DEBUG_LOG(m_point.x(), m_point.y(), m_point.x() + m_size.width(), m_point.y() + m_size.height());
     glFlush();
 }
