0def31d51580560734d07f3944ef8332caf17b2b
zhouxiang <zhouxiang@pset.suntec.net>
2016-04-15 14:47:23 +0800

    add painter device class

---
+++ graphic/engine/include/moos_paintdevice.h
@@ -0,0 +1,20 @@
+#ifndef MOOS_PAINTDEVICE_H
+#define MOOS_PAINTDEVICE_H
+
+#include "moos_defines.h"
+
+
+DEFINE_NAMESPACE_MOOS_BEGIN
+
+
+class MoosPaintDevice
+{
+public:
+private:
+
+
+};
+
+DEFINE_NAMESPACE_MOOS_END
+
+#endif // MOOS_PAINTDEVICE_H
